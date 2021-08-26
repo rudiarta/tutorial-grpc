611fbdff58121a4f78becdffb75bdc9e824bef90
rudiarta <rudiarta321@gmail.com>
2021-08-26 14:25:55 +0800

    with tls/ssl

--- main.go
+++ main.go
@@ -5,8 +5,11 @@ import (
 	"code/model"
 	c "code/src/client"
 	ss "code/src/server"
+	"crypto/tls"
+	"crypto/x509"
 	"flag"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net"
 
@@ -14,24 +17,61 @@ import (
 	"google.golang.org/grpc/credentials"
 )
 
+const (
+	serverCertFile   = "cert/server-cert.pem"
+	serverKeyFile    = "cert/server-key.pem"
+	clientCACertFile = "cert/ca-cert.pem"
+)
+
+func loadTLSCredentials() (credentials.TransportCredentials, error) {
+	// Load certificate of the CA who signed client's certificate
+	pemClientCA, err := ioutil.ReadFile(clientCACertFile)
+	if err != nil {
+		return nil, err
+	}
+
+	certPool := x509.NewCertPool()
+	if !certPool.AppendCertsFromPEM(pemClientCA) {
+		return nil, fmt.Errorf("failed to add client CA's certificate")
+	}
+
+	// Load server's certificate and private key
+	serverCert, err := tls.LoadX509KeyPair(serverCertFile, serverKeyFile)
+	if err != nil {
+		return nil, err
+	}
+
+	// Create the credentials and return it
+	config := &tls.Config{
+		Certificates: []tls.Certificate{serverCert},
+		ClientAuth:   tls.RequireAndVerifyClientCert,
+		ClientCAs:    certPool,
+	}
+
+	return credentials.NewTLS(config), nil
+}
+
 func main() {
 
 	cmd := flag.String("option", "", "")
 	flag.Parse()
 
 	if *cmd == `server` {
+
+		serverOptions := []grpc.ServerOption{}
+		tlsCredentials, err := loadTLSCredentials()
+		if err != nil {
+			log.Printf("cannot load TLS credentials: %w", err)
+		}
+
+		serverOptions = append(serverOptions, grpc.Creds(tlsCredentials))
+
 		ls, err := net.Listen("tcp", fmt.Sprintf("localhost:%s", common.PORT))
 		if err != nil {
 			log.Fatalf("failed to listen: %v", err)
 		}
-		creds, err := credentials.NewClientTLSFromFile("./cert/server.crt", "./cert/server.key")
-		if err != nil {
-			log.Fatalf("failed to listen: %v", err)
-		}
 
-		opts := []grpc.ServerOption{grpc.Creds(creds)}
-
-		grpcServer := grpc.NewServer(opts...)
+		grpcServer := grpc.NewServer(serverOptions...)
 		model.RegisterAccountManagementServer(grpcServer, ss.NewAccountServer())
 		err = grpcServer.Serve(ls)
 		if err != nil {
--- src/client/simply_rpc.go
+++ src/client/simply_rpc.go
@@ -4,19 +4,53 @@ import (
 	"code/common"
 	"code/model"
 	"context"
+	"crypto/tls"
+	"crypto/x509"
 	"fmt"
+	"io/ioutil"
 	"log"
 
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials"
 )
 
-func CreateAccount() {
-	creds, err := credentials.NewClientTLSFromFile("./cert/server.crt", "./cert/server.key")
+func loadTLSCredentials() (credentials.TransportCredentials, error) {
+	// Load certificate of the CA who signed server's certificate
+	pemServerCA, err := ioutil.ReadFile("cert/ca-cert.pem")
 	if err != nil {
-		log.Fatalf("failed to Dial: %v", err)
+		return nil, err
 	}
-	conn, err := grpc.Dial(fmt.Sprintf("localhost:%s", common.PORT), grpc.WithTransportCredentials(creds))
+
+	certPool := x509.NewCertPool()
+	if !certPool.AppendCertsFromPEM(pemServerCA) {
+		return nil, fmt.Errorf("failed to add server CA's certificate")
+	}
+
+	// Load client's certificate and private key
+	clientCert, err := tls.LoadX509KeyPair("cert/client-cert.pem", "cert/client-key.pem")
+	if err != nil {
+		return nil, err
+	}
+
+	// Create the credentials and return it
+	config := &tls.Config{
+		Certificates: []tls.Certificate{clientCert},
+		RootCAs:      certPool,
+	}
+
+	return credentials.NewTLS(config), nil
+}
+
+func CreateAccount() {
+	transportOption := grpc.WithInsecure()
+	tlsCredentials, err := loadTLSCredentials()
+	if err != nil {
+		log.Fatal("cannot load TLS credentials: ", err)
+	}
+
+	transportOption = grpc.WithTransportCredentials(tlsCredentials)
+
+	conn, err := grpc.Dial(fmt.Sprintf("localhost:%s", common.PORT), transportOption)
 	if err != nil {
 		log.Fatalf("failed to Dial: %v", err)
 	}
@@ -36,11 +70,15 @@ func CreateAccount() {
 }
 
 func CreateAccountBulk() {
-	creds, err := credentials.NewClientTLSFromFile("./cert/server.crt", "./cert/server.key")
+	transportOption := grpc.WithInsecure()
+	tlsCredentials, err := loadTLSCredentials()
 	if err != nil {
-		log.Fatalf("failed to Dial: %v", err)
+		log.Fatal("cannot load TLS credentials: ", err)
 	}
-	conn, err := grpc.Dial(fmt.Sprintf("localhost:%s", common.PORT), grpc.WithTransportCredentials(creds))
+
+	transportOption = grpc.WithTransportCredentials(tlsCredentials)
+
+	conn, err := grpc.Dial(fmt.Sprintf("localhost:%s", common.PORT), transportOption)
 	if err != nil {
 		log.Fatalf("failed to Dial: %v", err)
 	}
